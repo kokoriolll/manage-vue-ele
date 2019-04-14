8ae80bb90032fbc00d21ddbea0e9f910cfaf7746
吴洪洋 <2830174881@qq.com>
2019-04-15 07:41:38 +0800

    完成静态页面

--- vue-element-admin/src/layout/components/AppMain.vue
+++ vue-element-admin/src/layout/components/AppMain.vue
@@ -25,12 +25,14 @@ export default {
 <style lang="scss" scoped>
 .app-main {
   /* 50= navbar  50  */
-  min-height: calc(100vh - 50px);
+  /* min-height: calc(100vh - 50px); */
   width: 100%;
   position: relative;
   overflow: hidden;
   margin-top: 64px;
-  padding: 20px 25px 0 25px;
+  padding: 20px 25px 40px 25px;
+  box-sizing: border-box;
+  background: #F0F2F5;
 }
 
 .fixed-header+.app-main {
@@ -38,11 +40,6 @@ export default {
 }
 
 .hasTagsView {
-  .app-main {
-    /* 84 = navbar + tags-view = 50 + 34 */
-    min-height: calc(100vh - 84px);
-  }
-
   .fixed-header+.app-main {
     padding-top: 84px;
   }
