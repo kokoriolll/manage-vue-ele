21d24b2c6537e39605b281bedb3e9e5c434742ef
lylylyly <lylylyly163.com>
2019-04-26 10:42:36 +0800

    优化store/userShow

--- vue-element-admin/src/store/modules/userManagement.js
+++ vue-element-admin/src/store/modules/userManagement.js
@@ -29,7 +29,6 @@ const actions = {
   }, payload) {
     let result = await addUsers(payload);
     commit('Tips', result)
-    // console.log(result,666666)
     return result;
   },
   //更新用户信息（用户名，用户密码，用户身份）
